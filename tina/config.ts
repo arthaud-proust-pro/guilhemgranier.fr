3cde4ef0695a7c8d78b69200322c50412816cfdb
Arthaud <arthaudp33@gmail.com>
2024-02-10 19:41:32 +0100

    feat: add tina cms

---
+++ tina/config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "pages",
+        path: "content",
+        match: {
+          include: "*/_index",
+        },
+        label: "Rubriques",
+
+        fields: [
+          {
+            name: "draft",
+            label: "Brouillon",
+            type: "boolean",
+            required: false,
+          },
+          {
+            name: "title",
+            label: "Titre",
+            type: "string",
+            isTitle: true,
+            required: true,
+          },
+          {
+            name: "description",
+            label: "Description",
+            type: "string",
+            required: false,
+            ui: {
+              component: "textarea",
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
